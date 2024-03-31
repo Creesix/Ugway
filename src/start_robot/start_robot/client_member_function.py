3f32a8675192e6d201fc421cfa5cf48b18fee68b
root <ousmanethiongane17@gmail.com>
2024-03-31 05:51:18 +0000

    Updating start_robot package

--- src/start_robot/start_robot/client_member_function.py
+++ src/start_robot/start_robot/client_member_function.py
@@ -4,21 +4,24 @@ import RPi.GPIO as GPIO
 import time
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Bool
 from start_robot_interface.srv import StrategyInt
 
 class StartClient(Node):
 
     def __init__(self):
         super().__init__('start_client')
-        self.srv_client = self.create_client(StrategyInt, 'start_cli')     
+        self.srv_client = self.create_client(StrategyInt, 'start')     
         while not self.srv_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Service not available, waiting again...')
         self.req = StrategyInt.Request()
+        self.stop_publisher = self.create_publisher(Bool, 'stop', 10)
 
     def send_request(self, strategy):
         self.req.strategy = strategy
         self.future = self.srv_client.call_async(self.req)
 
+# Recherche du port USB lié à l'écran
 def find_screen_port():
     stlink_ports = [port.device for port in serial.tools.list_ports.comports() if "ST-Link" in port.description]
     if stlink_ports:
@@ -29,26 +32,31 @@ def find_screen_port():
 def main(args=None):
     rclpy.init(args=args)
     start_client = StartClient()
-
+    
+    # Connexion série
     port = find_screen_port() 
     baudrate = 115200  
     timeout = 1
+
+    # Broche de la tirette
     start_pin = 26
 
     if port:
         ser = serial.Serial(port, baudrate, timeout=timeout)
     else: 
-        minimal_client.get_logger().info("No screen device found on target !")
+        start_client.get_logger().info("No screen device found on target !")
         exit()
-
+    
+    # Lecture de la stratégie
     while True:
         received_data = ser.readline().decode().strip()
         if len(received_data) != 0:
-            strategy = received_data
+            strategy = int(received_data)
             break
 
     ser.close()
-
+    
+    # Initialisation de la tirette
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(start_pin, GPIO.IN)
     previous_state = GPIO.input(start_pin)
@@ -58,11 +66,16 @@ def main(args=None):
         
         if current_state != previous_state:
             start_client.send_request(strategy)
-            time.sleep(3)
-            minimal_client.get_logger().info("Match timeout !")
+            
+            # Durée d'un match
+            time.sleep(89)
+            msg = Bool()
+            msg.data = True
+            start_client.get_logger().info("Match timeout !")
+            start_client.stop_publisher.publish(msg)
             break
         
-        time.sleep(0.1)
+        time.sleep(0.01)
 
     GPIO.cleanup()
     rclpy.spin_once(start_client)
--- src/start_robot/start_robot/service_member_function.py
+++ src/start_robot/start_robot/service_member_function.py
@@ -7,21 +7,20 @@ class StartService(Node):
 
     def __init__(self):
         super().__init__('start_service')
-        self.srv = self.create_service(StrategyInt, 'start_srv', self.add_three_ints_callback)      
-
-    def add_three_ints_callback(self, request, response):
+        self.srv = self.create_service(StrategyInt, 'start', self.strategy_callback)      
+    
+    # Récupération de la stratégie
+    def strategy_callback(self, request, response):
         response.strategy = request.strategy                                          
-        self.get_logger().info('Incoming strategy\na: %d' % (request.strategy))  
+        self.get_logger().info('Incoming strategy: %d' % (request.strategy))  
 
         return response
 
 def main(args=None):
+
     rclpy.init(args=args)
-
     start_service = StartService()
-
     rclpy.spin(start_service)
-
     rclpy.shutdown()
 
 if __name__ == '__main__':
